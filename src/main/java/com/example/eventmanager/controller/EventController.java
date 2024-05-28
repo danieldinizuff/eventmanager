3463cb8b0decd1b72dc4c83a9f4eaa753f223b4a
Daniel <danieldinizuff@gmail.com>
2024-05-28 20:59:51 -0300

    Adicionando Swagger a aplicação

--- src/main/java/com/example/eventmanager/controller/EventController.java
+++ src/main/java/com/example/eventmanager/controller/EventController.java
@@ -3,6 +3,10 @@ package com.example.eventmanager.controller;
 
 import com.example.eventmanager.model.Event;
 import com.example.eventmanager.service.EventService;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,11 +14,13 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/events")
+@Api(value = "API de Gerenciamento de Eventos", description = "Endpoints para Gerenciamento de Eventos")
 public class EventController {
     @Autowired
     private EventService eventService;
 
     @PostMapping
+    @ApiOperation(value = "Lista todos os eventos", notes = "Retorna uma lista de todos os eventos")
     public ResponseEntity<Event> createEvent(@RequestBody Event event) {
         Event createdEvent = eventService.createEvent(event);
         return new ResponseEntity<>(createdEvent, HttpStatus.CREATED);
